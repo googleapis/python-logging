0da6ac6cf6c749c67d07fc8898eec319e2b61c6d
Daniel Sanche <d.sanche14@gmail.com>
2020-10-13 15:48:24 -0700

    removed python 2.7 tests

--- noxfile.py
+++ noxfile.py
@@ -97,7 +97,7 @@ def default(session, django_dep=('django',)):
     )
 
 
-@nox.session(python=['2.7', '3.5', '3.6', '3.7'])
+@nox.session(python=['3.5', '3.6', '3.7'])
 def unit(session):
     """Run the unit test suite."""
 
@@ -114,7 +114,7 @@ def unit(session):
         default(session)
 
 
-@nox.session(python=['2.7', '3.6'])
+@nox.session(python=['3.6'])
 def system(session):
     """Run the system test suite."""
 
