af33451d0a68e3818595912f8c6b262ddb2a97ab
Daniel Sanche <d.sanche14@gmail.com>
2020-10-27 16:20:01 -0700

    blacken noxfile

--- noxfile.py
+++ noxfile.py
@@ -154,19 +154,21 @@ def cover(session):
 def docs(session):
     """Build the docs for this library."""
 
-    session.install('-e', '.')
-    session.install('sphinx', 'alabaster', 'recommonmark')
+    session.install("-e", ".")
+    session.install("sphinx", "alabaster", "recommonmark")
 
-    shutil.rmtree(os.path.join('docs', '_build'), ignore_errors=True)
+    shutil.rmtree(os.path.join("docs", "_build"), ignore_errors=True)
     session.run(
-        'sphinx-build',
-        '-W',  # warnings as errors
-        '-T',  # show full traceback on exception
-        '-N',  # no colors
-        '-b', 'html',
-        '-d', os.path.join('docs', '_build', 'doctrees', ''),
-        os.path.join('docs', ''),
-        os.path.join('docs', '_build', 'html', ''),
+        "sphinx-build",
+        "-W",  # warnings as errors
+        "-T",  # show full traceback on exception
+        "-N",  # no colors
+        "-b",
+        "html",
+        "-d",
+        os.path.join("docs", "_build", "doctrees", ""),
+        os.path.join("docs", ""),
+        os.path.join("docs", "_build", "html", ""),
     )
 
 
