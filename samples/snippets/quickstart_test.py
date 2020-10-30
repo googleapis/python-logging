googleapis/python-language

Quickstart tests [(#569)](https://github.com/GoogleCloudPlatform/python-docs-samples/issues/569)
* Add tests for quickstarts
* Update secrets

samples/snippets/cloud-client/quickstart_test.py
@@ -1,0 +1,22 @@
+# Copyright 2016 Google Inc. All Rights Reserved.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+
+import quickstart
+
+
+def test_quickstart(capsys):
+    quickstart.run_quickstart()
+    out, _ = capsys.readouterr()
+    assert 'Sentiment' in out

googleapis/python-logging

samples/snippets/quickstart_test.py
@@ -1,0 +1,22 @@
+# Copyright 2016 Google Inc. All Rights Reserved.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+
+import quickstart
+
+
+def test_quickstart(capsys):
+    quickstart.run_quickstart()
+    out, _ = capsys.readouterr()
+    assert 'Logged' in out