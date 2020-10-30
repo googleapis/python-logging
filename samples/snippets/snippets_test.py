googleapis/python-bigquery

Move to google-cloud [(#544)](https://github.com/GoogleCloudPlatform/python-docs-samples/issues/544)

samples/snippets/snippets_test.py
@@ -12,7 +12,7 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-from gcloud import bigquery
+from google.cloud import bigquery
 import pytest
 
 import snippets

googleapis/python-logging

samples/snippets/snippets_test.py
@@ -12,8 +12,8 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-from gcloud import logging
 from gcp.testing import eventually_consistent
+from google.cloud import logging
 import pytest
 
 import snippets