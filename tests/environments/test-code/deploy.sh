530ace882104306594e4cbc855c5444a3f920bf6
Daniel Sanche <d.sanche14@gmail.com>
2020-11-24 16:49:00 -0800

    gae standard pubsub

--- tests/environments/test-code/deploy.sh
+++ tests/environments/test-code/deploy.sh
@@ -142,7 +142,7 @@ deploy_functions() {
 }
 
 deploy_ae_standard() {
-  local SCRIPT="${1:-test_flask.py}"
+  local SCRIPT="${1:-router.py}"
   local RUNTIME="${2:-python37}"
   # set up deployment directory
   # copy over local copy of library
@@ -151,16 +151,24 @@ deploy_ae_standard() {
     --exclude docs --exclude __pycache__
   # copy test scripts
   cp $SCRIPT_DIR/$SCRIPT $TMP_DIR/main.py
+  cp $SCRIPT_DIR/logger_tests.py $TMP_DIR/
   echo  "-e ./python-logging" | cat $SCRIPT_DIR/requirements.txt - > $TMP_DIR/requirements.txt
   # build app.yaml
   cat <<EOF > $TMP_DIR/app.yaml
     runtime: $RUNTIME
+    env_variables:
+      ENABLE_FLASK: "true"
 EOF
   # deploy
   pushd $TMP_DIR
     gcloud app deploy -q
     gcloud app browse
   popd
+  GAE_HOST=https://$(gcloud app describe --format="value(defaultHostname)")
+  gcloud pubsub subscriptions create gae-standard \
+    --topic logging-test \
+    --push-endpoint $GAE_HOST \
+    --ack-deadline 10
 }
 
 deploy_ae_flex_python() {
