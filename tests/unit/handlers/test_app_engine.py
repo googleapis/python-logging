69c0d396159bfee857825368ce0c1900595f2a9d
Daniel Sanche <d.sanche14@gmail.com>
2020-12-11 13:30:00 -0800

    added comment

--- tests/unit/handlers/test_app_engine.py
+++ tests/unit/handlers/test_app_engine.py
@@ -92,10 +92,10 @@ class TestAppEngineHandler(unittest.TestCase):
         expected_trace_id = f"projects/{self.PROJECT}/traces/{trace_id}"
         get_request_patch = mock.patch(
             "google.cloud.logging_v2.handlers.app_engine.get_request_data",
-            return_value=({"request_url": "test"}, "trace-test"),
+            return_value=(expected_http_request, trace_id),
         )
         with get_request_patch:
-
+            # library integrations mocked to return test data
             client = mock.Mock(project=self.PROJECT, spec=["project"])
             handler = self._make_one(client, transport=_Transport)
             gae_resource = handler.get_gae_resource()
