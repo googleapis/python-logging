b57ce3a819bc66b5d8a8ee20ca97dabfe41d42ee
Daniel Sanche <d.sanche14@gmail.com>
2021-02-25 14:44:20 -0800

    fixed variable name

--- google/cloud/logging_v2/handlers/handlers.py
+++ google/cloud/logging_v2/handlers/handlers.py
@@ -159,9 +159,9 @@ def setup_logging(
     logger = logging.getLogger()
     logger.setLevel(log_level)
     # disable built-in StreamHandlers in GAE Flex to avoid duplicate logs
-    for h in root_logger.handlers:
+    for h in logger.handlers:
         if isinstance(h, logging.StreamHandler):
-            root_logger.removeHandler(h)
+            logger.removeHandler(h)
     logger.addHandler(handler)
     for logger_name in all_excluded_loggers:
         logger = logging.getLogger(logger_name)
