bd90217cf9ab8a037dd48e1c93da54a36cca5bba
Daniel Sanche <d.sanche14@gmail.com>
2021-03-01 17:05:21 -0800

    blacken tests

--- tests/unit/handlers/test__monitored_resources.py
+++ tests/unit/handlers/test__monitored_resources.py
@@ -18,31 +18,48 @@ import mock
 import os
 
 
-from google.cloud.logging_v2.handlers._monitored_resources import _create_functions_resource
-from google.cloud.logging_v2.handlers._monitored_resources import _create_app_engine_resource
-from google.cloud.logging_v2.handlers._monitored_resources import _create_kubernetes_resource
-from google.cloud.logging_v2.handlers._monitored_resources import _create_cloud_run_resource
-from google.cloud.logging_v2.handlers._monitored_resources import _create_compute_resource
-from google.cloud.logging_v2.handlers._monitored_resources import _create_global_resource
+from google.cloud.logging_v2.handlers._monitored_resources import (
+    _create_functions_resource,
+)
+from google.cloud.logging_v2.handlers._monitored_resources import (
+    _create_app_engine_resource,
+)
+from google.cloud.logging_v2.handlers._monitored_resources import (
+    _create_kubernetes_resource,
+)
+from google.cloud.logging_v2.handlers._monitored_resources import (
+    _create_cloud_run_resource,
+)
+from google.cloud.logging_v2.handlers._monitored_resources import (
+    _create_compute_resource,
+)
+from google.cloud.logging_v2.handlers._monitored_resources import (
+    _create_global_resource,
+)
 from google.cloud.logging_v2.handlers._monitored_resources import detect_resource
 from google.cloud.logging_v2.handlers import _monitored_resources
 from google.cloud.logging_v2.resource import Resource
 
+
 class Test_Create_Resources(unittest.TestCase):
 
-    PROJECT = 'test-project'
-    LOCATION = 'test-location'
-    NAME = 'test-name'
-    CLUSTER = 'test-cluster'
-    VERSION = '1'
-    CONFIG = 'test-config'
+    PROJECT = "test-project"
+    LOCATION = "test-location"
+    NAME = "test-name"
+    CLUSTER = "test-cluster"
+    VERSION = "1"
+    CONFIG = "test-config"
 
     def _mock_metadata(self, endpoint):
-        if (endpoint == _monitored_resources._ZONE_ID
-                or endpoint == _monitored_resources._REGION_ID):
+        if (
+            endpoint == _monitored_resources._ZONE_ID
+            or endpoint == _monitored_resources._REGION_ID
+        ):
             return self.LOCATION
-        elif (endpoint == _monitored_resources._GKE_CLUSTER_NAME
-                or endpoint == _monitored_resources._GCE_INSTANCE_ID):
+        elif (
+            endpoint == _monitored_resources._GKE_CLUSTER_NAME
+            or endpoint == _monitored_resources._GCE_INSTANCE_ID
+        ):
             return self.NAME
         else:
             return None
@@ -58,10 +75,10 @@ class Test_Create_Resources(unittest.TestCase):
             legacy_func_resource = _create_functions_resource(self.PROJECT)
 
             self.assertIsInstance(legacy_func_resource, Resource)
-            self.assertEqual(legacy_func_resource.type, 'cloud_function')
-            self.assertEqual(legacy_func_resource.labels['project_id'], self.PROJECT)
-            self.assertEqual(legacy_func_resource.labels['function_name'], self.NAME)
-            self.assertEqual(legacy_func_resource.labels['region'], self.LOCATION)
+            self.assertEqual(legacy_func_resource.type, "cloud_function")
+            self.assertEqual(legacy_func_resource.labels["project_id"], self.PROJECT)
+            self.assertEqual(legacy_func_resource.labels["function_name"], self.NAME)
+            self.assertEqual(legacy_func_resource.labels["region"], self.LOCATION)
 
     def test_create_modern_functions_resource(self):
         patch = mock.patch(
@@ -73,10 +90,10 @@ class Test_Create_Resources(unittest.TestCase):
             func_resource = _create_functions_resource(self.PROJECT)
 
             self.assertIsInstance(func_resource, Resource)
-            self.assertEqual(func_resource.type, 'cloud_function')
-            self.assertEqual(func_resource.labels['project_id'], self.PROJECT)
-            self.assertEqual(func_resource.labels['function_name'], self.NAME)
-            self.assertEqual(func_resource.labels['region'], self.LOCATION)
+            self.assertEqual(func_resource.type, "cloud_function")
+            self.assertEqual(func_resource.labels["project_id"], self.PROJECT)
+            self.assertEqual(func_resource.labels["function_name"], self.NAME)
+            self.assertEqual(func_resource.labels["region"], self.LOCATION)
 
     def test_create_kubernetes_resource(self):
 
@@ -88,10 +105,10 @@ class Test_Create_Resources(unittest.TestCase):
             resource = _create_kubernetes_resource(self.PROJECT)
 
             self.assertIsInstance(resource, Resource)
-            self.assertEqual(resource.type, 'k8s_container')
-            self.assertEqual(resource.labels['project_id'], self.PROJECT)
-            self.assertEqual(resource.labels['cluster_name'], self.NAME)
-            self.assertEqual(resource.labels['location'], self.LOCATION)
+            self.assertEqual(resource.type, "k8s_container")
+            self.assertEqual(resource.labels["project_id"], self.PROJECT)
+            self.assertEqual(resource.labels["cluster_name"], self.NAME)
+            self.assertEqual(resource.labels["location"], self.LOCATION)
 
     def test_compute_resource(self):
         patch = mock.patch(
@@ -102,10 +119,10 @@ class Test_Create_Resources(unittest.TestCase):
         with patch:
             resource = _create_compute_resource(self.PROJECT)
             self.assertIsInstance(resource, Resource)
-            self.assertEqual(resource.type, 'gce_instance')
-            self.assertEqual(resource.labels['project_id'], self.PROJECT)
-            self.assertEqual(resource.labels['instance_id'], self.NAME)
-            self.assertEqual(resource.labels['zone'], self.LOCATION)
+            self.assertEqual(resource.type, "gce_instance")
+            self.assertEqual(resource.labels["project_id"], self.PROJECT)
+            self.assertEqual(resource.labels["instance_id"], self.NAME)
+            self.assertEqual(resource.labels["zone"], self.LOCATION)
 
     def test_cloud_run_resource(self):
         patch = mock.patch(
@@ -118,12 +135,12 @@ class Test_Create_Resources(unittest.TestCase):
         with patch:
             resource = _create_cloud_run_resource(self.PROJECT)
             self.assertIsInstance(resource, Resource)
-            self.assertEqual(resource.type, 'cloud_run_revision')
-            self.assertEqual(resource.labels['project_id'], self.PROJECT)
-            self.assertEqual(resource.labels['service_name'], self.NAME)
-            self.assertEqual(resource.labels['revision_name'], self.VERSION)
-            self.assertEqual(resource.labels['configuration_name'], self.CONFIG)
-            self.assertEqual(resource.labels['location'], self.LOCATION)
+            self.assertEqual(resource.type, "cloud_run_revision")
+            self.assertEqual(resource.labels["project_id"], self.PROJECT)
+            self.assertEqual(resource.labels["service_name"], self.NAME)
+            self.assertEqual(resource.labels["revision_name"], self.VERSION)
+            self.assertEqual(resource.labels["configuration_name"], self.CONFIG)
+            self.assertEqual(resource.labels["location"], self.LOCATION)
 
     def test_app_engine_resource(self):
         patch = mock.patch(
@@ -135,31 +152,34 @@ class Test_Create_Resources(unittest.TestCase):
         with patch:
             resource = _create_app_engine_resource(self.PROJECT)
             self.assertIsInstance(resource, Resource)
-            self.assertEqual(resource.type, 'gae_app')
-            self.assertEqual(resource.labels['project_id'], self.PROJECT)
-            self.assertEqual(resource.labels['module_id'], self.NAME)
-            self.assertEqual(resource.labels['version_id'], self.VERSION)
-            self.assertEqual(resource.labels['zone'], self.LOCATION)
+            self.assertEqual(resource.type, "gae_app")
+            self.assertEqual(resource.labels["project_id"], self.PROJECT)
+            self.assertEqual(resource.labels["module_id"], self.NAME)
+            self.assertEqual(resource.labels["version_id"], self.VERSION)
+            self.assertEqual(resource.labels["zone"], self.LOCATION)
 
     def test_global_resource(self):
         resource = _create_global_resource(self.PROJECT)
         self.assertIsInstance(resource, Resource)
-        self.assertEqual(resource.type, 'global')
-        self.assertEqual(resource.labels['project_id'], self.PROJECT)
+        self.assertEqual(resource.type, "global")
+        self.assertEqual(resource.labels["project_id"], self.PROJECT)
+
 
 class Test_Resource_Detection(unittest.TestCase):
 
-    PROJECT = 'test-project'
+    PROJECT = "test-project"
 
     def _mock_k8s_metadata(self, endpoint):
-        if (endpoint == _monitored_resources._GKE_CLUSTER_NAME
-                or endpoint == _monitored_resources._GCE_INSTANCE_ID):
+        if (
+            endpoint == _monitored_resources._GKE_CLUSTER_NAME
+            or endpoint == _monitored_resources._GCE_INSTANCE_ID
+        ):
             return "TRUE"
         else:
             return None
 
     def _mock_gce_metadata(self, endpoint):
-        if  endpoint == _monitored_resources._GCE_INSTANCE_ID:
+        if endpoint == _monitored_resources._GCE_INSTANCE_ID:
             return "TRUE"
         else:
             return None
@@ -169,10 +189,10 @@ class Test_Resource_Detection(unittest.TestCase):
 
     def test_detect_appengine(self):
         for env in _monitored_resources._GAE_ENV_VARS:
-            os.environ[env] = 'TRUE'
+            os.environ[env] = "TRUE"
         resource = detect_resource(self.PROJECT)
         self.assertIsInstance(resource, Resource)
-        self.assertEqual(resource.type, 'gae_app')
+        self.assertEqual(resource.type, "gae_app")
 
     def test_detect_kubernetes(self):
         patch = mock.patch(
@@ -182,28 +202,28 @@ class Test_Resource_Detection(unittest.TestCase):
         with patch:
             resource = detect_resource(self.PROJECT)
             self.assertIsInstance(resource, Resource)
-            self.assertEqual(resource.type, 'k8s_container')
+            self.assertEqual(resource.type, "k8s_container")
 
     def test_detect_functions(self):
         for env in _monitored_resources._FUNCTION_ENV_VARS:
-            os.environ[env] = 'TRUE'
+            os.environ[env] = "TRUE"
         resource = detect_resource(self.PROJECT)
         self.assertIsInstance(resource, Resource)
-        self.assertEqual(resource.type, 'cloud_function')
+        self.assertEqual(resource.type, "cloud_function")
 
     def test_detect_legacy_functions(self):
         for env in _monitored_resources._LEGACY_FUNCTION_ENV_VARS:
-            os.environ[env] = 'TRUE'
+            os.environ[env] = "TRUE"
         resource = detect_resource(self.PROJECT)
         self.assertIsInstance(resource, Resource)
-        self.assertEqual(resource.type, 'cloud_function')
+        self.assertEqual(resource.type, "cloud_function")
 
     def test_detect_cloud_run(self):
         for env in _monitored_resources._CLOUD_RUN_ENV_VARS:
-            os.environ[env] = 'TRUE'
+            os.environ[env] = "TRUE"
         resource = detect_resource(self.PROJECT)
         self.assertIsInstance(resource, Resource)
-        self.assertEqual(resource.type, 'cloud_run_revision')
+        self.assertEqual(resource.type, "cloud_run_revision")
 
     def test_detect_compute_engine(self):
         patch = mock.patch(
@@ -213,10 +233,9 @@ class Test_Resource_Detection(unittest.TestCase):
         with patch:
             resource = detect_resource(self.PROJECT)
             self.assertIsInstance(resource, Resource)
-            self.assertEqual(resource.type, 'gce_instance')
+            self.assertEqual(resource.type, "gce_instance")
 
     def test_detection_unknown(self):
         resource = detect_resource(self.PROJECT)
         self.assertIsInstance(resource, Resource)
-        self.assertEqual(resource.type, 'global')
-
+        self.assertEqual(resource.type, "global")
