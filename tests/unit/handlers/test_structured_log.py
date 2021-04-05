570bc2d2d0d969d8d2968ca7c6ed260ef812ea1c
Daniel Sanche <d.sanche14@gmail.com>
2021-04-05 14:39:45 -0700

    added a test with http_request data

--- tests/unit/handlers/test_structured_log.py
+++ tests/unit/handlers/test_structured_log.py
@@ -27,6 +27,18 @@ class TestStructuredLogHandler(unittest.TestCase):
     def _make_one(self, *args, **kw):
         return self._get_target_class()(*args, **kw)
 
+    @staticmethod
+    def create_app():
+        import flask
+
+        app = flask.Flask(__name__)
+
+        @app.route("/")
+        def index():
+            return "test flask trace"  # pragma: NO COVER
+
+        return app
+
     def test_ctor_defaults(self):
         handler = self._make_one()
         self.assertIsNone(handler.name)
@@ -58,6 +70,12 @@ class TestStructuredLogHandler(unittest.TestCase):
                 "file": pathname,
                 "line": str(lineno),
                 "function": func
+            },
+            "httpRequest": {
+                "requestMethod": "",
+                "requestUrl": "",
+                "userAgent": "",
+                "protocol": ""
             }
         }
         handler.filter(record)
@@ -65,3 +83,38 @@ class TestStructuredLogHandler(unittest.TestCase):
         result =  json.loads(handler.format(record))
         for (key, value) in expected_payload.items():
             self.assertEqual(value, result[key])
+
+    def test_format_with_request(self):
+        import logging
+        import json
+
+        handler = self._make_one()
+        logname = "loggername"
+        message = "hello world，嗨 世界"
+        record = logging.LogRecord(
+            logname, logging.INFO, "", 0, message, None, None
+        )
+        expected_path = "http://testserver/123"
+        expected_agent = "Mozilla/5.0"
+        body_content = "test"
+        expected_payload = {
+            "httpRequest": {
+                "requestMethod": "PUT",
+                "requestUrl": expected_path,
+                "userAgent": expected_agent,
+                "protocol": "HTTP/1.1"
+            }
+        }
+
+        app = self.create_app()
+        with app.test_client() as c:
+            c.put(
+                path=expected_path,
+                data="body",
+                headers={"User-Agent": expected_agent},
+            )
+            handler.filter(record)
+            payload = handler.format(record)
+            result =  json.loads(handler.format(record))
+            for (key, value) in expected_payload.items():
+                self.assertEqual(value, result[key])
