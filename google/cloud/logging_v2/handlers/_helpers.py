f9c36645b7711ed41547ee18af29c309ff1de755
Daniel Sanche <d.sanche14@gmail.com>
2021-05-11 13:55:09 -0700

    added missing content_length block

--- google/cloud/logging_v2/handlers/_helpers.py
+++ google/cloud/logging_v2/handlers/_helpers.py
@@ -96,6 +96,13 @@ def get_request_data_from_django():
     if request is None:
         return None, None, None
 
+    # convert content_length to int if it exists
+    content_length = None
+    try:
+        content_length = int(request.META.get(_DJANGO_CONTENT_LENGTH))
+    except (ValueError, TypeError):
+        content_length = None
+
     # build http_request
     http_request = {
         "requestMethod": request.method,
