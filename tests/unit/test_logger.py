eb8c549488f36b128ca9416002b5dc34f8a96e8a
Daniel Sanche <d.sanche14@gmail.com>
2021-05-17 13:47:45 -0700

    logger and cloudlogginghandler detect defaults from environment

--- tests/unit/test_logger.py
+++ tests/unit/test_logger.py
@@ -99,11 +99,15 @@ class TestLogger(unittest.TestCase):
         self.assertIs(batch.client, client2)
 
     def test_log_empty_defaults_w_default_labels(self):
+        from google.cloud.logging_v2.handlers._monitored_resources import (
+            detect_resource,
+        )
+
         DEFAULT_LABELS = {"foo": "spam"}
         ENTRIES = [
             {
                 "logName": "projects/%s/logs/%s" % (self.PROJECT, self.LOGGER_NAME),
-                "resource": {"type": "global", "labels": {}},
+                "resource": detect_resource(self.PROJECT)._to_dict(),
                 "labels": DEFAULT_LABELS,
             }
         ]
@@ -170,7 +174,10 @@ class TestLogger(unittest.TestCase):
         self.assertEqual(api._write_entries_called_with, (ENTRIES, None, None, None))
 
     def test_log_text_defaults(self):
-        RESOURCE = {"type": "global", "labels": {}}
+        from google.cloud.logging_v2.handlers._monitored_resources import (
+            detect_resource,
+        )
+        RESOURCE = detect_resource(self.PROJECT)._to_dict()
         TEXT = "TEXT"
         ENTRIES = [
             {
@@ -188,8 +195,11 @@ class TestLogger(unittest.TestCase):
         self.assertEqual(api._write_entries_called_with, (ENTRIES, None, None, None))
 
     def test_log_text_w_unicode_and_default_labels(self):
+        from google.cloud.logging_v2.handlers._monitored_resources import (
+            detect_resource,
+        )
         TEXT = "TEXT"
-        RESOURCE = {"type": "global", "labels": {}}
+        RESOURCE = detect_resource(self.PROJECT)._to_dict()
         DEFAULT_LABELS = {"foo": "spam"}
         ENTRIES = [
             {
@@ -265,8 +275,11 @@ class TestLogger(unittest.TestCase):
         self.assertEqual(api._write_entries_called_with, (ENTRIES, None, None, None))
 
     def test_log_struct_defaults(self):
+        from google.cloud.logging_v2.handlers._monitored_resources import (
+            detect_resource,
+        )
         STRUCT = {"message": "MESSAGE", "weather": "cloudy"}
-        RESOURCE = {"type": "global", "labels": {}}
+        RESOURCE = detect_resource(self.PROJECT)._to_dict()
         ENTRIES = [
             {
                 "logName": "projects/%s/logs/%s" % (self.PROJECT, self.LOGGER_NAME),
@@ -283,8 +296,11 @@ class TestLogger(unittest.TestCase):
         self.assertEqual(api._write_entries_called_with, (ENTRIES, None, None, None))
 
     def test_log_struct_w_default_labels(self):
+        from google.cloud.logging_v2.handlers._monitored_resources import (
+            detect_resource,
+        )
         STRUCT = {"message": "MESSAGE", "weather": "cloudy"}
-        RESOURCE = {"type": "global", "labels": {}}
+        RESOURCE = detect_resource(self.PROJECT)._to_dict()
         DEFAULT_LABELS = {"foo": "spam"}
         ENTRIES = [
             {
@@ -360,6 +376,9 @@ class TestLogger(unittest.TestCase):
         self.assertEqual(api._write_entries_called_with, (ENTRIES, None, None, None))
 
     def test_log_proto_defaults(self):
+        from google.cloud.logging_v2.handlers._monitored_resources import (
+            detect_resource,
+        )
         import json
         from google.protobuf.json_format import MessageToJson
         from google.protobuf.struct_pb2 import Struct, Value
@@ -369,7 +388,7 @@ class TestLogger(unittest.TestCase):
             {
                 "logName": "projects/%s/logs/%s" % (self.PROJECT, self.LOGGER_NAME),
                 "protoPayload": json.loads(MessageToJson(message)),
-                "resource": {"type": "global", "labels": {}},
+                "resource": detect_resource(self.PROJECT)._to_dict(),
             }
         ]
         client = _Client(self.PROJECT)
@@ -381,6 +400,9 @@ class TestLogger(unittest.TestCase):
         self.assertEqual(api._write_entries_called_with, (ENTRIES, None, None, None))
 
     def test_log_proto_w_default_labels(self):
+        from google.cloud.logging_v2.handlers._monitored_resources import (
+            detect_resource,
+        )
         import json
         from google.protobuf.json_format import MessageToJson
         from google.protobuf.struct_pb2 import Struct, Value
@@ -391,7 +413,7 @@ class TestLogger(unittest.TestCase):
             {
                 "logName": "projects/%s/logs/%s" % (self.PROJECT, self.LOGGER_NAME),
                 "protoPayload": json.loads(MessageToJson(message)),
-                "resource": {"type": "global", "labels": {}},
+                "resource": detect_resource(self.PROJECT)._to_dict(),
                 "labels": DEFAULT_LABELS,
             }
         ]
