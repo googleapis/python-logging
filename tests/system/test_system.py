b30f9e798e73f51349984982d5a27cd226349ef1
Daniel Sanche <d.sanche14@gmail.com>
2021-12-07 12:07:36 -0800

    added more test cases

--- tests/system/test_system.py
+++ tests/system/test_system.py
@@ -454,6 +454,7 @@ class TestLogging(unittest.TestCase):
 
             self.assertEqual(len(entries), 1)
             self.assertIsNone(entries[0].payload)
+            self.assertFalse(entries[0].trace_sampled)
 
     def test_log_struct_logentry_data(self):
         logger = Config.CLIENT.logger(self._logger_name("log_w_struct"))
@@ -473,6 +474,7 @@ class TestLogging(unittest.TestCase):
         self.assertEqual(entries[0].severity, "WARNING")
         self.assertEqual(entries[0].trace, JSON_PAYLOAD["trace"])
         self.assertEqual(entries[0].span_id, JSON_PAYLOAD["span_id"])
+        self.assertFalse(entries[0].trace_sampled)
 
     def test_log_handler_async(self):
         LOG_MESSAGE = "It was the worst of times"
