737b12114120834c8f72e78ea0ae5ff5af3b3c23
Bu Sun Kim <8822365+busunkim96@users.noreply.github.com>
2022-03-10 17:28:07 -0700

    chore: also update google-cloud-core

--- setup.py
+++ setup.py
@@ -30,7 +30,7 @@ version = "1.15.1"
 release_status = 'Development Status :: 5 - Production/Stable'
 dependencies = [
     "google-api-core[grpc] >= 1.31.5, <3.0.0dev,!=2.0.*,!=2.1.*,!=2.2.*,!=2.3.0",
-    "google-cloud-core >= 1.1.0, < 2.0dev",
+    "google-cloud-core >= 1.1.0, < 3.0dev",
 ]
 extras = {
 }
