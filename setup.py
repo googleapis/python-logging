412b4b8935ad9c1361318b95b18c3039e77c7561
Anthonios Partheniou <partheniou@google.com>
2022-07-07 16:22:38 +0000

    fix(deps): require google-api-core 2.8.0

--- setup.py
+++ setup.py
@@ -32,9 +32,6 @@ dependencies = [
     "google-api-core[grpc] >= 2.8.0, <3.0.0dev",
     "google-cloud-appengine-logging>=0.1.0, <2.0.0dev",
     "google-cloud-audit-log >= 0.1.0, < 1.0.0dev",
-    # NOTE: Maintainers, please do not require google-api-core>=2.x.x
-    # Until this issue is closed
-    # https://github.com/googleapis/google-cloud-python/issues/10566
     "google-cloud-core >= 1.4.1, <3.0.0dev",
     "grpc-google-iam-v1 >=0.12.4, <1.0.0dev",
     "proto-plus >= 1.15.0, <2.0.0dev",
