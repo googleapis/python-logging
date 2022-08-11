6d3eec8f178c6315d51ee2992de80dddb31a1216
Daniel Sanche <d.sanche14@gmail.com>
2022-08-11 12:52:41 -0700

    owlbot adds our env vars

--- owlbot.py
+++ owlbot.py
@@ -82,6 +82,13 @@ s.replace(
     'pass_down_envvars+=(\n    "ENVIRONMENT"\n    "RUNTIME"'
 )
 
+for test_type in ["continuous", "presubmit", "release", "samples", "docs"]:
+    s.move(
+        ".kokoro/common_env_vars.cfg",
+        f".kokoro/{test_type}/common.cfg",
+        merge=lambda src, dst, _, : f"{dst}\n{src}",
+    )
+
 # don't lint environment tests
 s.replace(
     ".flake8",
