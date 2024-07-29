e7b56b9fb0b7eb118bfb6cb9717d7c7d2d919f86
Kevin Zheng <kevzheng@google.com>
2024-07-29 20:01:19 +0000

    linting

--- google/cloud/logging_v2/client.py
+++ google/cloud/logging_v2/client.py
@@ -38,7 +38,7 @@ from google.cloud.logging_v2.handlers._monitored_resources import (
     _GKE_RESOURCE_TYPE,
     _GCF_RESOURCE_TYPE,
     _RUN_RESOURCE_TYPE,
-    _CLOUD_RUN_JOB_RESOURCE_TYPE
+    _CLOUD_RUN_JOB_RESOURCE_TYPE,
 )
 
 from google.cloud.logging_v2.logger import Logger
@@ -382,7 +382,7 @@ class Client(ClientWithProject):
             _GKE_RESOURCE_TYPE,
             _GCF_RESOURCE_TYPE,
             _RUN_RESOURCE_TYPE,
-            _CLOUD_RUN_JOB_RESOURCE_TYPE
+            _CLOUD_RUN_JOB_RESOURCE_TYPE,
         ]
 
         if (
