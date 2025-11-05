43713f6d4e9ac7a401b5b8fb018f599a76c6a0d6
Linchin <lingqing.gan@gmail.com>
2025-11-05 19:51:03 +0000

    remove python 3.7 and 3.8 from kokoro unit test

--- noxfile.py
+++ noxfile.py
@@ -35,8 +35,6 @@ LINT_PATHS = ["docs", "google", "tests", "noxfile.py", "setup.py"]
 DEFAULT_PYTHON_VERSION = "3.8"
 
 UNIT_TEST_PYTHON_VERSIONS: List[str] = [
-    "3.7",
-    "3.8",
     "3.9",
     "3.10",
     "3.11",
